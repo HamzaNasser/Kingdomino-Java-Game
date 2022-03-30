ea92c76e610ffaf869359a9ea0df33d8497a5ee1
Cuza <Cuza@192.168.2.11>
2022-03-30 16:25:36 -0230

    Implemented Territory class

--- KingDomino/src/Player.java
+++ KingDomino/src/Player.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class Player {
@@ -7,11 +9,24 @@ public class Player {
 	private String name;
 	private int score;
 	private Color color;
+	private ArrayList<Territory> territories;
 	
 	public Player(String name, Color color) {
 		this.name = name;
 		score = 0;
 		this.color = color;
 	}
+	
+	public void addTerritory(Territory territory) {
+		territories.add(territory);
+		score = calculateScore();
+	}
 
+	private int calculateScore() {
+		int scoreCount = 0;
+		for (Territory territory : territories) {
+			scoreCount += territory.getTerritoryPoints();
+		}
+		return scoreCount;
+	}
 }
