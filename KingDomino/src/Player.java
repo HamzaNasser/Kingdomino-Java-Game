cf46249a5ddeb0b3c65001e7afb7460e5ec30d25
Cuza <Cuza@192.168.2.11>
2022-03-31 14:05:02 -0230

    Removed Territory class and updated Player class.

--- KingDomino/src/Player.java
+++ KingDomino/src/Player.java
@@ -9,7 +9,6 @@ public class Player {
 	private String name;
 	private int score;
 	private Color color;
-	private ArrayList<Territory> territories;
 	
 	public Player(String name, Color color) {
 		this.name = name;
@@ -17,16 +16,4 @@ public class Player {
 		this.color = color;
 	}
 	
-	public void addTerritory(Territory territory) {
-		territories.add(territory);
-		score = calculateScore();
-	}
-
-	private int calculateScore() {
-		int scoreCount = 0;
-		for (Territory territory : territories) {
-			scoreCount += territory.getTerritoryPoints();
-		}
-		return scoreCount;
-	}
 }
